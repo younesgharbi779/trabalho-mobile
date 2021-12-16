242b35dba8f5dc4a00573d25ab9d107d0db36880
younesgharbi779 <93740913+younesgharbi779@users.noreply.github.com>
2021-12-15 21:55:28 -0300

    o

--- lojinha/src/pages/Produtos/index.jsx
+++ lojinha/src/pages/Produtos/index.jsx
@@ -23,4 +23,12 @@ export default function Produtos({navigation, route}){
         
       console.log("terminei a funcao")
     };
-     
+    useEffect(()=>{handleClick()},[])
+    const jogo = produto
+  return(
+      <View style={styles.container}>
+          <Header home={()=> navigation.goBack("Home")}/>
+          <CardDetalhe img={jogo.imagens} descricao={jogo.descricao} preco={jogo.vlUnitario} /> 
+      </View>
+  )
+}
