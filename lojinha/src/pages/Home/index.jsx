63b52945b83e360a8abe603b9a9b3b93ae29c7b3
younesgharbi779 <93740913+younesgharbi779@users.noreply.github.com>
2021-12-15 21:51:58 -0300

    ok

--- lojinha/src/pages/Home/index.jsx
+++ lojinha/src/pages/Home/index.jsx
@@ -7,33 +7,3 @@ import Carrossel from "../../components/Carrossel";
 import Categorias from "../../components/Categorias";
 import Card from "../../components/Card";
 import axios from "axios";
-
-export default function Home({ navigation }) {
-  const [produto,setProduto] = useState([]);
-
-  const handleClick = async () => {
-
-    await axios({
-      method: "GET",
-      url:"https://api-da-lojinha.herokuapp.com/produtos",
-
-    }).then(response =>{
-      setProduto(response.data)
-    })
-  };
-  useEffect(() => { handleClick() }, [])
-
-
-  return (
-    <ScrollView style={styles.containerHome}>
-    <Header carrinho={()=> navigation.navigate("Carrinho")} />
-      <Carrossel />
-      <Categorias />
-      <View style={styles.container}>
-      {produto?.map((produto)=>{
-        return( <Card style={styles.container} key={produto.id} uri={produto.imagens} price={produto.vlUnitario} titulo={produto.nome} detalhe={() => {navigation.navigate("DetalheProdutos",{itemId:produto.id})}}/>)
-      })}
-      </View>
-    </ScrollView>
-  );
-}
